367a6e082ef691d005b65fdcbd1d6165a0d471bd
Pashupati Nath Vidyarthi <254213285+pashupatinathvidyarthi@users.noreply.github.com>
2026-03-31 12:25:08 +0530

    Java

--- lec04.java
+++ lec04.java
@@ -1,7 +1,6 @@
-public class lec04
-{
-    public static void main(String[]args)
-    {
+public class lec04 {
+    public static void main(String[] args) {
         System.out.println("Hello World");
     }
+
 }
